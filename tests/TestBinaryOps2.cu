3011d59f28d05cb173f51f303d27734c6c6ffe17
Sebastian Weiß <sebastian.weiss_germering@web.de>
2017-11-03 17:23:37 +0100

    WIP: extended tests for binary ops

--- tests/TestBinaryOps2.cu
+++ tests/TestBinaryOps2.cu
@@ -9,3 +9,19 @@
 // Test for broadcasting
 // TODO
 
+using namespace cuMat;
+
+template<int _Rows, int _Cols, int _Batches>
+Matrix<int, (_Rows>1) ? Dynamic : _Rows, (_Cols>1) ? Dynamic : _Cols, (_Batches>1) ? Dynamic : _Batches, ColumnMajor>
+fromArray(int a[_Batches][_Cols][_Rows])
+{
+    typedef Matrix<int, (_Rows > 1) ? Dynamic : _Rows, (_Cols > 1) ? Dynamic : _Cols, (_Batches > 1) ? Dynamic : _Batches, ColumnMajor> mt;
+    mt m(_Rows, _Cols, _Batches);
+    m.copyFromHost(a);
+    return m;
+}
+
+TEST_CASE("fromArray", "[binary]")
+{
+    
+}
