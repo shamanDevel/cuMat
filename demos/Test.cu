ff334c379d2e8a3f8f8b5dd5926609ecc0934d5a
Sebastian Weiß <sebastian.weiss_germering@web.de>
2017-11-25 13:52:17 +0100

    added quick start guide and benchmark documentation

--- demos/Test.cu
+++ demos/Test.cu
@@ -5,36 +5,22 @@
 #include <chrono>
 #include <vector>
 
-using namespace cuMat;
-using namespace std;
+#include <iostream>
+#include <cuMat/Core>
 
-int main(int argc, char* args[])
+int main()
 {
-    std::vector<int> sizes = { 100, 1000, 10000, 100000, 1000000, 10000000 };
-    for (int size : sizes) {
-        //cuMat
-        VectorXf v(size);
-        cudaEvent_t start, stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-        cudaDeviceSynchronize();
-        cudaEventRecord(start, cuMat::Context::current().stream());
-        //VectorXf r = v * 5;
-        (v * 5).eval();
-        cudaEventRecord(stop, cuMat::Context::current().stream());
-        cudaEventSynchronize(stop);
-        float elapsedCuMat;
-        cudaEventElapsedTime(&elapsedCuMat, start, stop);
-        //Eigen
-        Eigen::VectorXf ve(size);
-        auto startE = std::chrono::steady_clock::now();
-        //Eigen::VectorXf re = ve * 5;
-        (ve * 5).eval();
-        auto stopE = std::chrono::steady_clock::now();
-        double elapsedEigen = std::chrono::duration_cast<std::chrono::duration<double>>(stopE - startE).count() * 1000;
+    cuMat::BMatrixXf mat(3, 3, 1);
+    cuMat::SimpleRandom rand(1);
+    rand.fillUniform(mat, 0.0f, 1.0f);
+    cuMat::BMatrixXf result = mat * 2;
+    std::cout << result << std::endl;
 
-        cout << "Size: " << size << endl;
-        cout << "Time cuMat: " << elapsedCuMat << " ms" << endl;
-        cout << "Time Eigen: " << elapsedEigen << " ms" << endl;
-    }
+    /*
+     * Example output:
+Matrix: rows=3 (dynamic), cols=3 (dynamic), batches=1 (dynamic), storage=Column-Major
+0.580378  1.35463  1.41331
+ 1.88441 0.766545  1.83773
+0.696211 0.899099 0.773809
+     */
 }
