90491cc074e64f0526576ed31a00044eef11e22c
Sebastian Weiss <sebastian13.weiss@tum.de>
2018-08-04 20:14:23 +0200

    WIP: started with CG

---
+++ cuMat/src/ConjugateGradient.h
@@ -0,0 +1,17 @@
+#ifndef __CUMAT_CONJUGATED_GRADIENT__
+#define __CUMAT_CONJUGATED_GRADIENT__
+
+#include "Macros.h"
+
+#include "Matrix.h"
+#include "UnaryOps.h"
+#include "BinaryOps.h"
+#include "ReductionOps.h"
+
+CUMAT_NAMESPACE_BEGIN
+
+
+
+CUMAT_NAMESPACE_END
+
+#endif
