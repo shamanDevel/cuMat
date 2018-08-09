205f1aa171c40b3cad528b75fbf1b78bae5b06b7
Sebastian Weiss <sebastian13.weiss@tum.de>
2018-08-09 10:21:42 +0200

    added compound-assignment to MatrixBlock

--- cuMat/src/MatrixBlock.h
+++ cuMat/src/MatrixBlock.h
@@ -19,7 +19,7 @@ namespace internal {
 			RowsAtCompileTime = _Rows,
 			ColsAtCompileTime = _Columns,
 			BatchesAtCompileTime = _Batches,
-            AccessFlags = ReadCwise | WriteCwise
+            AccessFlags = ReadCwise | WriteCwise | (traits<_MatrixType>::AccessFlags & RWCwise ? RWCwise : 0) | (traits<_MatrixType>::AccessFlags & RWCwiseRef ? RWCwiseRef : 0)
 		};
         typedef CwiseSrcTag SrcTag;
         typedef DenseDstTag DstTag;
@@ -142,6 +142,38 @@ public:
 		coeff(i, j, k, -1) = newValue;
 	}
 
+	/**
+	* \brief Access to the linearized coefficient, read-only.
+	* The format of the indexing depends on whether this
+	* matrix is column major (ColumnMajorBit) or row major (RowMajorBit).
+	* Requirement of \c AccessFlags::RWCwise .
+	* \param idx the linearized index of the entry.
+	* \return the entry at that index
+	*/
+	__device__ CUMAT_STRONG_INLINE _Scalar getRawCoeff(Index idx) const
+	{
+		//This method is quite ineffective at the moment, since it has to convert the values back to row,col,batch
+		Index i, j, k;
+		index(idx, i, j, k);
+		return coeff(i, j, k, -1);
+	}
+
+	/**
+	* \brief Access to the linearized coefficient, read-only.
+	* The format of the indexing depends on whether this
+	* matrix is column major (ColumnMajorBit) or row major (RowMajorBit).
+	* Requirement of \c AccessFlags::RWCwiseRef .
+	* \param index the linearized index of the entry.
+	* \return the entry at that index
+	*/
+	__device__ CUMAT_STRONG_INLINE _Scalar& rawCoeff(Index idx)
+	{
+		//This method is quite ineffective at the moment, since it has to convert the values back to row,col,batch
+		Index i, j, k;
+		index(idx, i, j, k);
+		return coeff(i, j, k, -1);
+	}
+
 	//ASSIGNMENT
 
 	template<typename Derived>
@@ -155,7 +187,58 @@ public:
 		return *this;
 	}
 
-    //TODO: add operator+=, -=, ....
+#define CUMAT_COMPOUND_ASSIGNMENT(op, mode)                                                             \
+    /**                                                                                                 \
+    * \brief Compound-assignment with evaluation, modifies this matrix in-place.                        \
+    * Warning: if this matrix shares the data with another matrix, this matrix is modified as well.     \
+    * If you don't intend this, call \ref makeExclusiveUse() first.                                     \
+    *                                                                                                   \
+    * No broadcasting is supported, use the verbose \code mat = mat + expr \endcode instead.            \
+    * Further, not all expressions might support inplace-assignment.                                    \
+    *                                                                                                   \
+    * \tparam Derived the type of the other expression                                                  \
+    * \param expr the other expression                                                                  \
+    * \return                                                                                           \
+    */                                                                                                  \
+    template<typename Derived>                                                                          \
+    CUMAT_STRONG_INLINE Type& op (const MatrixBase<Derived>& expr)                                      \
+    {                                                                                                   \
+        CUMAT_ASSERT_ARGUMENT(rows() == expr.rows());													\
+		CUMAT_ASSERT_ARGUMENT(cols() == expr.cols());													\
+		CUMAT_ASSERT_ARGUMENT(batches() == expr.batches());												\
+		internal::Assignment<Type, Derived, AssignmentMode:: mode, internal::DenseDstTag, typename internal::traits<Derived>::SrcTag>::assign(*this, expr.derived());	\
+		return *this;                                                                                 \
+    }
+
+    CUMAT_COMPOUND_ASSIGNMENT(operator+=, ADD)
+    CUMAT_COMPOUND_ASSIGNMENT(operator-=, SUB)
+    //CUMAT_COMPOUND_ASSIGNMENT(operator*=, MUL) //multiplication is ambigious: do you want cwise or matrix multiplication?
+    CUMAT_COMPOUND_ASSIGNMENT(operator/=, DIV)
+    CUMAT_COMPOUND_ASSIGNMENT(operator%=, MOD)
+    CUMAT_COMPOUND_ASSIGNMENT(operator&=, AND)
+    CUMAT_COMPOUND_ASSIGNMENT(operator|=, OR)
+
+	/**
+	* \brief Explicit overloading of \c operator*= for scalar right hand sides.
+	* This is needed to disambiguate the difference between component-wise operations and matrix operations.
+	* All other compount-assignment operators (+=, -=, /=, ...) act component-wise.
+	*
+	* \tparam S the type of the scalar
+	* \param scalar the scalar value
+	* \return *this
+	*/
+	template<
+		typename S,
+		typename T = typename std::enable_if<CUMAT_NAMESPACE internal::canBroadcast<Scalar, S>::value, Type>::type >
+	CUMAT_STRONG_INLINE T& operator*= (const S& scalar)
+	{
+		//Type::Constant(rows(), cols(), batches(), scalar).template evalTo<Type, AssignmentMode::MUL>(*this);
+		using Expr = decltype(Type::Constant(rows(), cols(), batches(), scalar));
+		internal::Assignment<Type, Expr, AssignmentMode::MUL, internal::DenseDstTag, typename internal::traits<Expr>::SrcTag>::assign(*this, Type::Constant(rows(), cols(), batches(), scalar));
+		return *this;
+	}
+
+#undef CUMAT_COMPOUND_ASSIGNMENT
 };
 
 
